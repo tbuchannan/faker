b06f73dc948d552efbca9aac50bb0d6c9f2853a3
Terence Buchannan <terencebuchannan@gmail.com>
2017-11-13 23:45:34 -0500

    Naruto Test: Add Tests

---
+++ test/test_faker_naruto.rb
@@ -0,0 +1,48 @@
+require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
+
+class TestFakerNaruto < Test::Unit::TestCase
+
+  def setup
+    @tester = Faker::Naruto
+  end
+
+  def test_character
+    assert @tester.character.match(/\w+/)
+  end
+
+  def test_kage
+    assert @tester.kage.match(/\w+/)
+  end
+
+  def test_genjutsu
+    assert @tester.genjutsu.match(/\w+/)
+  end
+
+  def test_ninjutsu
+    assert @tester.ninjutsu.match(/\w+/)
+  end
+
+  def test_taijutsu
+    assert @tester.taijutsu.match(/\w+/)
+  end
+
+  def test_quote
+    assert @tester.quote.match(/\w+/)
+  end
+
+  def test_villain
+    assert @tester.villain.match(/\w+/)
+  end
+
+  def test_location
+    assert @tester.location.match(/\w+/)
+  end
+
+  def test_summon
+    assert @tester.summon.match(/\w+/)
+  end
+
+  def test_tailed_beast
+    assert @tester.tailed_beast.match(/\w+/)
+  end
+end
